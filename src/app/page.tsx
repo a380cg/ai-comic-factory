630c70e313b86332f01b5be345dcd1dcfe1438f2
a380cg <gogzee01+1@gmail.com>
2025-03-02 12:24:22 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,7 @@ export default async function IndexPage() {
 
         </TooltipProvider>
 
+        <iframe data-aa='2377679' src='//acceptable.a-ads.com/2377679' style='border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe>
         <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-WH4MGSHS" />
         <Script id="google-analytics">
           {`
